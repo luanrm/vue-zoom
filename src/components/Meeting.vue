c672568730bc85ce3d0f5cc99c0172657af93b67
Luan Macêdo <luan@MacBook-Pro-de-Eduardo.local>
2020-07-09 15:23:50 -0300

    evolucoes

--- src/components/Meeting.vue
+++ src/components/Meeting.vue
@@ -1,67 +1,77 @@
 <template>
-  <div class="ReactModal__Body--open">
-    <!-- added on import -->
-    <div id="zmmtg-root"></div>
-    <div id="aria-notify-area"></div>
-
-    <!-- added on meeting init -->
-    <div class="ReactModalPortal"></div>
-    <div class="ReactModalPortal"></div>
-    <div class="ReactModalPortal"></div>
-    <div class="ReactModalPortal"></div>
-    <div class="global-pop-up-box"></div>
-    <div class="sharer-controlbar-container sharer-controlbar-container--hidden"></div>
+  <div>
+    {{msg}}
   </div>
 </template>
 
 <script>
 import { ZoomMtg } from "@zoomus/websdk";
+import axios from "axios";
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+
 export default {
   name: "Meeting",
   props: {
     msg: String
   },
+  data() {
+    return {
+      signatureEndpoint: "http://localhost:4000/",
+      apiKey: "Ym1CE8fuTpycvEi9eW7OvA",
+      meetingNumber: 3392892467,
+      leaveUrl: "http://localhost:8080/",
+      userName: "Firstname Lastname",
+      userEmail: "", // required for webinar
+      passWord: "802676", // if required
+      role: 0 // 1 for host; 0 for attendee or webinar
+    };
+  },
   mounted() {
-    this.ZoomInit();
+    this.getSignature();
   },
   methods: {
-    ZoomInit() {
-      ZoomMtg.setZoomJSLib(
-        "https://dmogdx0jrul3u.cloudfront.net/1.7.9/lib",
-        "/av"
-      );
-      ZoomMtg.preLoadWasm();
-      ZoomMtg.prepareJssdk();
-
-      const meetConfig = {
-        apiKey: "",
-        meetingNumber: "123456789",
-        leaveUrl: "http://192.168.1.102:8080/",
-        userName: "Firstname Lastname",
-        userEmail: "firstname.lastname@yoursite.com", // required for webinar
-        passWord: "password", // if required
-        role: 0 // 1 for host; 0 for attendee or webinar
-      };
-
+    startMeeting(signature) {
       ZoomMtg.init({
-        leaveUrl: meetConfig.leaveUrl,
+        leaveUrl: this.leaveUrl,
         isSupportAV: true,
-        success: function() {
+        success: success => {
+          console.log(success);
+
           ZoomMtg.join({
-            signature: "cripto",
-            apiKey: meetConfig.apiKey,
-            meetingNumber: meetConfig.meetingNumber,
-            userName: meetConfig.userName,
-            // Email required for Webinars
-            userEmail: meetConfig.userEmail,
-            // password optional; set by Host
-            password: meetConfig.passWord
+            signature: signature,
+            meetingNumber: this.meetingNumber,
+            userName: this.userName,
+            apiKey: this.apiKey,
+            userEmail: this.userEmail,
+            passWord: this.passWord,
+            success: success => {
+              console.log(success);
+            },
+            error: error => {
+              console.log(error);
+            }
           });
         },
-        error(res) {
-          console.log(res);
+        error: error => {
+          console.log(error);
         }
       });
+    },
+
+    getSignature() {
+      axios
+        .post(this.signatureEndpoint, {
+          meetingNumber: this.meetingNumber,
+          role: this.role
+        })
+        .then(response => {
+          this.startMeeting(response.data.signature);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 };
